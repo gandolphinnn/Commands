b13fca42b1f984522101e8973fdf21b73eb3b362
Luca Gandolfi <luca.gandolfi@naar.com>
2026-04-08 12:47:10 +0200

    Pray: accept folder parameter

--- Pray.bat
+++ Pray.bat
@@ -1,1 +1,12 @@
+@echo off
+if "%~1"=="" goto launch
+if exist "D:\Progetti\%~1\" (
+    cd /d "D:\Progetti\%~1"
+) else if exist "D:\Personale\%~1\" (
+    cd /d "D:\Personale\%~1"
+) else (
+    echo Folder "%~1" not found in D:\Progetti or D:\Personale
+    exit /b 1
+)
+:launch
 claude --dangerously-skip-permissions
