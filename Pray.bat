133ea8d724219ad35173949f40f221ab106b778d
Luca Gandolfi <luca.gandolfi@naar.com>
2026-05-12 16:58:24 +0200

    pray enhancement

--- Pray.bat
+++ Pray.bat
@@ -1,11 +1,18 @@
 @echo off
-if "%~1"=="" goto launch
-if exist "D:\Progetti\%~1\" (
-    cd /d "D:\Progetti\%~1"
+if "%~1"=="" (
+	rem No parameter provided, defaulting to just D:\
+	cd /d "D:\"
+) else if exist "D:\%~1\" (
+    rem Folder exists directly under D:\
+	cd /d "D:\%~1"
+) else if exist "D:\Progetti\%~1\" (
+    rem Folder exists under D:\Progetti
+	cd /d "D:\Progetti\%~1"
 ) else if exist "D:\Personale\%~1\" (
-    cd /d "D:\Personale\%~1"
+    rem Folder exists under D:\Personale
+	cd /d "D:\Personale\%~1"
 ) else (
-    echo Folder "%~1" not found in D:\Progetti or D:\Personale
+    echo Folder "%~1" not found in D:\, D:\Progetti or D:\Personale
     exit /b 1
 )
 :launch
